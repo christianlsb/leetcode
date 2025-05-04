8bcfdabf145fddcbfb579f258bab49310206f579
Christian Lisboa <102741778+christianlsb@users.noreply.github.com>
2025-05-04 12:26:27 -0300

    new solution!

--- busca_binaria/index.py
+++ busca_binaria/index.py
@@ -1,17 +1,12 @@
-def pesquisa_binaria(lista, item):
-    baixo = 0
-    alto = len(lista) - 1
-
-    while baixo <= alto:
-        meio = int((baixo + alto) / 2) 
-        chute = lista[meio]
-        if chute == item:
-            return meio
-        if chute > item:
-            alto = meio - 1
-        else:
-            baixo = meio + 1 
-    return None 
-
-lista_de_numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-print(pesquisa_binaria(lista_de_numeros, 5))
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) -1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid
+        return -1
